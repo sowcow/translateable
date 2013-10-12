bc347ff1ccf36887930517571d50945e811b8c4f
Alexander K <xpyro@ya.ru>
2013-10-12 16:53:52 +0600

    $ rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ancestry::Application.config.secret_key_base = 'c33acb3e3470cdd204cd6e38f8645e3a1bad109d7f5269011146fccc0f448c9b70cde3699e35bfcffa2529965b070d1de9a353169861beb3f9505893f2b8212b'
