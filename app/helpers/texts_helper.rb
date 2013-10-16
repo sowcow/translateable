9379e2c55587007bf8fa00c16c26590124cc6348
Alexander K <xpyro@ya.ru>
2013-10-16 11:48:29 +0600

    all-stuff

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,2 +1,7 @@
 module TextsHelper
+  def header_link_to title, object, options = {}
+    options = { class: 'tiny secondary round button' }
+              .merge options
+    link_to title, object, options
+  end
 end
