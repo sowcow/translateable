8e56d74308300cef9b459dc392aeec66ee1443b8
Alexander K <xpyro@ya.ru>
2013-10-16 17:00:29 +0600

    layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def the_title
+    if content_for?(:title)
+      content_for :title
+    else
+      if content_for?(:header)
+        content_for :header
+      else
+        '...'
+      end
+    end
+  end
 end
